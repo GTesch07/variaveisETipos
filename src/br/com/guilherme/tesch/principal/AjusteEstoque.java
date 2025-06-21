a722ded8a5598af1365bd42c6c3f637e1d340357
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-06-21 20:35:03 -0300

    Exercício para formatar valor double para int.

---
+++ src/br/com/guilherme/tesch/principal/AjusteEstoque.java
@@ -0,0 +1,11 @@
+package br.com.guilherme.tesch.principal;
+
+public class AjusteEstoque {
+    public static void main(String[]args){
+        double valorDouble = 19.5;
+
+        int valorFormatado = (int) valorDouble;
+
+        System.out.println("O valor inteiro do produto é: " + valorFormatado);
+    }
+}
